80fd76beff29305a5b9c7c53736745ea2462419c
bwrightprojects <bwrightprojects@gmail.com>
2015-04-12 11:50:50 -0700

    Fixed Legend on bar
    The legend on the bar graph wasn’t rendering correctly on my machine or
    png

--- plot4.R
+++ plot4.R
@@ -29,6 +29,7 @@ data$Datetime <- as.POSIXct(datetime)
 
 ## Plot 4
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+##par(mfrow=c(2,2))
 with(data, {
         plot(Global_active_power~Datetime, type="l", 
              ylab="Global Active Power (kilowatts)", xlab="")
@@ -39,7 +40,9 @@ with(data, {
         lines(Sub_metering_2~Datetime,col='Red')
         lines(Sub_metering_3~Datetime,col='Blue')
         legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-               legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+               cex=.35, pt.cex=1, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+        
         plot(Global_reactive_power~Datetime, type="l", 
              ylab="Global Rective Power (kilowatts)",xlab="")
 })

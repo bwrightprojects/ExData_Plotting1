61ee4fa8381cadeff3585e2f2e0a69634eb4ba11
bwrightprojects <bwrightprojects@gmail.com>
2015-04-12 11:11:11 -0700

    Plot2 update
    Updated plot2.r and plot2.png.  Also replaced plot1.png with proper
    “par” settings.

---
+++ plot2.R
@@ -0,0 +1,35 @@
+
+## Download the dataset
+#  setwd("./Project1")
+#  dataurl <- "https://github.com/rdpeng/ExData_Plotting1"
+#  datafile <- file.path(getwd(), "household_power_consumption.zip")
+#  download.file(dataurl, datafile, method = "curl")
+##--unzip(datafile, exdir = "./Data")
+
+## This file is for loading the large dataset.
+## Getting full dataset
+
+allData <- read.table("./Data/household_power_consumption.txt",
+                   header = TRUE,
+                   sep = ";",
+                   colClasses = c("character", "character", rep("numeric",7)),
+                   na = "?")
+
+allData$Date <- as.Date(allData$Date, format="%d/%m/%Y")
+
+## Subsetting the data for the two days 02/01/2007 - 02/02/2007
+data <- subset(allData, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# free up memory
+rm(allData)
+
+## Convert the dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 2
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
